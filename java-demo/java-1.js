eaf0e324e1b25c378fa00d0134483db3cecda063
paresh-zampadiya <paresh.phonelcdparts@gmail.com>
2025-06-09 07:08:58 +0000

    variable javascript ex :- const , let , var

---
+++ java-demo/java-1.js
@@ -0,0 +1,15 @@
+const accountId = 555;
+let accountName = "Paresh";
+var accountPassword = "111";
+accountCity = "surat";
+
+let acountState ;
+
+
+// accountId = 11;
+accountName = "zamp;adiya";
+accountPassword = "555";
+accountCity = "ahd";
+
+console.log([accountId , accountName , accountPassword , accountCity]);
+console.table([accountId , accountName , accountPassword , accountCity , acountState]);
