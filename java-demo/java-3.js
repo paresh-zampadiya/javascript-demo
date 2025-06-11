2c717829b18826674ae48dd57b7285d97bdec4e9
paresh-zampadiya <paresh.phonelcdparts@gmail.com>
2025-06-11 09:55:47 +0000

    datatype convesion

---
+++ java-demo/java-3.js
@@ -0,0 +1,25 @@
+// let scrol = "paresh"
+
+// console.log(typeof scrol);
+// console.log(typeof (scrol));
+
+
+// let valuOfscrol = Number(scrol)
+// console.log(typeof valuOfscrol);
+// console.log(valuOfscrol);
+
+
+// 33 => 33
+// 33abc => NaN
+// true => 1 false => 0
+// null => NaN
+
+let logined = 0;
+
+let isLogin = Boolean(logined);
+
+console.log(isLogin);
+console.log(logined);
+
+// true => 1 false => 0
+// "" => dalse "paresh" => true
