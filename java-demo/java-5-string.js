1da6eb16d76a06be5fc6eb732690ba86e37b613d
paresh-zampadiya <paresh.phonelcdparts@gmail.com>
2025-07-08 09:11:13 +0000

    string

---
+++ java-demo/java-5-string.js
@@ -0,0 +1,19 @@
+const name = "paresh"
+const id = 50
+
+// console.log( name + id + "ooo");
+
+console.log(`my name is ${name} and my id ${id}`);
+
+
+const gameName = new String('marco')
+
+console.log(gameName[2]);
+console.log(gameName.length);
+console.log(gameName.__proto__);
+console.log(gameName.toUpperCase());
+
+
+const newName = "    paresh    "
+console.log(newName); // trim space none
+console.log(newName.trim()); // trim space none
