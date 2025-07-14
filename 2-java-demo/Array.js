73def096fed5ed116a496d2e3368f730f5098569
paresh-zampadiya <paresh.phonelcdparts@gmail.com>
2025-07-14 12:58:09 +0000

    array part-1

---
+++ 2-java-demo/Array.js
@@ -0,0 +1,44 @@
+// Array 
+
+// const myArr = ["abc", "def", "pqr"]
+// console.log(myArr[1]);
+
+const number = new Array(1, 2, 3, 4)
+// console.log(number);
+
+// const number2 = new Array(1, 2, 3, 4)
+// console.log(number2.toString());
+
+
+// const name = {firstname:"paresh" , lastname:"zampadiya", age:20 };
+// console.log(name.firstname)
+
+
+// array method
+
+// number.push(6)
+// number.pop()
+
+// number.unshift(10)
+// number.shift()
+
+// console.log(number.includes(8));
+// console.log(number.indexOf(4));
+
+// const newArr = number.join()
+// console.log(number);
+// console.log(newArr);
+
+// console.log(number);
+
+// slice , splice
+
+console.log("A " , number);
+
+const myn = number.slice(1,3)
+console.log(myn);
+
+console.log("B " , number);
+
+const myn2 = number.splice(1,3)
+console.log(myn2);
--- java-demo/java-7-date-time.js
+++ java-demo/java-7-date-time.js
@@ -8,3 +8,43 @@ console.log(myDate.toISOString()); // 2025-07-09T11:45:00.979Z
 console.log(myDate.toJSON()); // 2025-07-09T11:45:00.979Z
 console.log(myDate.toLocaleDateString()); //7/9/2025
 console.log(myDate.toLocaleString()); // 7/9/2025, 11:45:00 AM
+
+// let myTodayDate = new Date(2025 , 6, 11)
+// console.log(myTodayDate.toDateString());  // Fri Jul 11 2025
+
+// let myTodayDate = new Date(2025 , 6, 11, 5, 20)
+// console.log(myTodayDate.toLocaleString());  // 7/11/2025, 5:20:00 AM
+
+// let myTodayDate = new Date("2025-5-10") // yyyy-mm-dd
+// console.log(myTodayDate.toLocaleDateString()); // 5/10/2025
+
+// let myTodayDate = new Date("2025-12-15") // yyyy-mm-dd
+// console.log(myTodayDate.toLocaleString()); // 5/10/2025, 12:00:00 AM
+
+// let myTodayDate = new Date("2025-5-10")
+// console.log(myTodayDate.getTime()); // 1746835200000
+
+// let mySetDate = Date.now()
+// console.log(mySetDate); // 1752148149659
+
+// let mySetDate = Date.now()
+// console.log(Date.now() / 1000); // 1752148238.995
+
+let mySetDate = Date.now()
+console.log(Math.floor(Date.now() / 1000)); // 1752148255
+
+let newDate = new Date()
+console.log(newDate); // 2025-07-11T04:58:54.285Z
+console.log(newDate.getDate()); // 11 date
+console.log(newDate.getDay()); // week day count = 5 - friday
+console.log(newDate.getMonth() + 1); // month 7 july
+console.log(newDate.getFullYear()); // 2025 year
+
+
+// `${newDate.getDay()} today `
+
+newDate.toLocaleString('default' , {
+    weekday : 'long'
+})
+
+console.log(newDate);
